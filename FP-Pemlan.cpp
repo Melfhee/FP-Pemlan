7bea543f7a4dbeff5172ade04507863e7302a94e
Melfhee <melindafauziyah1@gmail.com>
2019-12-09 23:45:23 +0700

    menambah case meni 1 dan 2

--- FP-Pemlan.cpp
+++ FP-Pemlan.cpp
@@ -13,7 +13,7 @@ struct pasien{
 };
  main(){
  	struct pasien pa[30];
-	int menu =-1;
+	int menu =-1, i=0, urutan;
 	while(menu!=5){
 	printf("=======================================\n");
 	printf("    APLIKASI PENDATAAN RUMAH SAKIT \n");
@@ -29,8 +29,41 @@ struct pasien{
 	printf ("======================================\n");
 	
 		switch(menu){
-			case 1:
+			case 1: 	printf("Masukkan Id Pasien			: ");
+						scanf("%d",&pa[i].id);
+						printf("\nMasukan Nama Pasien		: ");
+						scanf("%s",&pa[i].nama);
+						printf("\nMasukan Penyakit Pasien	: ");
+						scanf("%s",&pa[i].penyakit);
+						printf("\nMasukan Nomor Kamar Pasien: ");
+						scanf("%d",&pa[i].nomor_kamar);
+						printf("\nMasukan Jenis Kamar		: ");
+						scanf("%s",&pa[i].jenis_kamar);
+						printf("\nMasukan Tanggal Masuk		: ");
+						scanf("%s",&pa[i].date);
+						i++;
+						system("cls");
 				break;
+			case 2 : if(i==0){
+					printf("\ndata kosong");
+					}else{
+						printf("=======================================\n");
+						printf("    Pengurutan Data Pasien Berdasarkan \n");
+						printf("=======================================\n");
+						printf(" 1. ID Pasien \n");
+						printf(" 2. Nama Pasien\n");
+						printf(" 3. Jenis Penyakit\n");
+						printf(" 4. Jenis Kamar\n");
+						printf(" 5. Nomor Kamar \n");
+						printf("======================================\n");
+						printf(" Masukkan Pilihan : ");
+						scanf("%d",&urutan);
+						printf ("======================================\n");
+						switch(urutan){
+							case 1: 
+							break;
+				}
+			}
 			}
 		}
 }
